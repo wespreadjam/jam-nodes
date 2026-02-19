ca665d59b0d0710a1935482b6261a9a0b6da227f
Jet Pham <jetthomaspham@gmail.com>
2026-02-19 10:41:53 -0800

    feat: add direnv environment with nix flake
    Adds a flake.nix file that adds nodejs and typescript development dependencies. Version locked with flake.lock and temp files are properly added .gitignore

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A Nix-flake-based Node.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs
+            typescript
+          ];
+        };
+      });
+}
